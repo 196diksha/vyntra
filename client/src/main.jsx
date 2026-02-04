c4eb284fd9e849c16e3150719e901ff8616d41bf
196diksha <bajpaidiksha25@gmail.com>
2026-02-04 16:26:43 +0530

    commit 04

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,9 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import './index.css'
 import App from './App'
+import axios from 'axios'
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE || ''
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
